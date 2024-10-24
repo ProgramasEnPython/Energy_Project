1d08c4969b0621f9e9a4f8a24efe3bb930a03a92
LCarlosVZ <lcarlosvazap@gmail.com>
2024-10-24 01:42:29 -0500

    Se agregó la opcion de seleccionar grafica

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 # Configuración de Flask-Mail para Gmail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'correo@gmail.com'  # Cambia por nuestra dirección de correo
-app.config['MAIL_PASSWORD'] = 'contraseña'  # Cambia por nuestra contraseña
+app.config['MAIL_USERNAME'] = 'inforpotenciasolar@gmail.com'  # Cambia por nuestra dirección de correo
+app.config['MAIL_PASSWORD'] = 'Potenciasolar2024'  # Cambia por nuestra contraseña
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
@@ -86,6 +86,9 @@ def calcular():
     region = request.form['region']
     area_disponible = float(request.form.get('area_disponible'))
 
+    # Obtener el tipo de gráficos seleccionados por el usuario
+    chart_types = request.form.getlist('chart_type[]')
+
     total_energy = 0  # Para almacenar el consumo total de energía
     appliance_data = []  # Para almacenar la energía consumida por cada electrodoméstico.
 
@@ -99,11 +102,22 @@ def calcular():
         appliance_data.append(
             (appliance, energy))  # La información de cada electrodoméstico se agrega a la lista appliance_data
 
-    filename = generate_chart(
-        appliance_data)  # Se llama a la función generate_chart() para generar un gráfico basado en los datos de consumo de los electrodomésticos, y se almacena el nombre del archivo generado.
+    # Generar gráficos según lo que seleccione el usuario
+    chart_filenames = []  # Lista para almacenar los nombres de los archivos de gráficos
 
-    chart_url = url_for('output_file',
-                        filename=filename)  # Se utiliza url_for() para crear una URL que apunta a la ruta que servirá el gráfico generado. Esto permite que la plantilla resultados.html acceda a la imagen.
+    if 'bar' in chart_types:
+        bar_chart = generate_chart(appliance_data, chart_type='bar')
+        chart_filenames.append(bar_chart)
+
+    if 'line' in chart_types:
+        line_chart = generate_chart(appliance_data, chart_type='line')
+        chart_filenames.append(line_chart)
+
+    if 'pie' in chart_types:
+        pie_chart = generate_chart(appliance_data, chart_type='pie')
+        chart_filenames.append(pie_chart)
+
+    chart_urls = [url_for('output_file', filename=filename) for filename in chart_filenames]
 
     send_email(email, appliance_data, total_energy, department,
                region)  # Se llama a la función send_email() para enviar un correo electrónico con los resultados del consumo energético y la información adicional.
@@ -131,7 +145,7 @@ def calcular():
     if area_total > area_disponible:
         # Si el área disponible es menor, se muestra una advertencia al usuario
         return render_template('resultados.html', total_energy=total_energy, appliance_data=appliance_data,
-                               chart_url=chart_url,
+                               chart_urls=chart_urls,
                                paneles_necesarios=paneles_necesarios,
                                area_total=area_total,
                                area_disponible=area_disponible,
@@ -139,7 +153,7 @@ def calcular():
     else:
         # Si el área disponible es suficiente, se continúa normalmente
         return render_template('resultados.html', total_energy=total_energy, appliance_data=appliance_data,
-                               chart_url=chart_url,
+                               chart_urls=chart_urls,
                                paneles_necesarios=paneles_necesarios,
                                area_total=area_total,
                                area_disponible=area_disponible,
@@ -156,8 +170,8 @@ def enviar_contacto():
 
     # Crear el mensaje de correo
     msg = Message('Nuevo mensaje de contacto de Potencia Solar',
-                  sender='correo@gmail.com',  # Remitente (cambiar esto por nuestro correo)
-                  recipients=['destinatario@gmail.com'])  # Destinatario (debe ser otro correo)
+                  sender='inforpotenciasolar@gmail.com',  # Remitente (cambiar esto por nuestro correo)
+                  recipients=['soportepotenciasolar@gmail.com'])  # Destinatario (debe ser otro correo)
 
     # Cuerpo del mensaje
     msg.body = f"""
@@ -184,8 +198,8 @@ def calculate_energy(appliance, quantity):
 
 def send_email(to_email, appliance_data, total_energy, department, region):
     """Envía un correo electrónico con el resultado del consumo energético."""
-    from_email = "correo@gmail.com"
-    from_password = "ponercontraseña"
+    from_email = "inforpotenciasolar@gmail.com"
+    from_password = "Potenciasolar2024"
 
     # Crear el mensaje
     msg = MIMEMultipart()
@@ -224,59 +238,41 @@ Cada tupla contiene el nombre de un electrodoméstico y su correspondiente consu
 """
 
 
-def generate_chart(appliance_data):
-    """
-    Aquí se utilizan comprensiones de listas para extraer los nombres de los electrodomésticos y sus
-    consumos de energía en dos listas separadas: appliances y energies.
-    """
+def generate_chart(appliance_data, chart_type='bar'):
     appliances = [appliance for appliance, energy in appliance_data]
     energies = [energy for appliance, energy in appliance_data]
-    """
-    Se establece el estilo del gráfico como 'ggplot'.
-    Se configura el tamaño de la figura del gráfico a 8 pulgadas de ancho y 6 pulgadas de alto.
-    Se crea el gráfico de barras utilizando plt.bar(), donde los nombres de los electrodomésticos se usan 
-    como etiquetas del eje X y sus consumos de energía como valores del eje Y.
-    Las barras tienen un color azul claro (#3498db), con un borde negro y un grosor de línea de 1.5.
-    """
+
     plt.style.use('ggplot')
-    plt.figure(figsize=(8, 6))
-    bars = plt.bar(appliances, energies, color='#3498db', edgecolor='black', linewidth=1.5)
+    plt.figure(figsize=(4, 4))
 
-    # Se añaden etiquetas a los ejes X e Y y un título al gráfico, con tamaños de fuente específicos.
-    plt.xlabel('Electrodoméstico', fontsize=14)
-    plt.ylabel('Consumo en kWh', fontsize=14)
-    plt.title('Consumo Energético', fontsize=16, fontweight='bold')
+    if chart_type == 'bar':
+        bars = plt.bar(appliances, energies, color='#3498db', edgecolor='black', linewidth=1.5)
+        plt.xlabel('Electrodoméstico', fontsize=14)
+        plt.ylabel('Consumo en kWh', fontsize=14)
+        plt.title('Consumo Energético - Barra', fontsize=16, fontweight='bold')
+        for bar in bars:
+            yval = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width() / 2, yval, f'{yval:.0f} kWh', va='bottom', ha='center', fontsize=14, color='black')
 
-    """
-    Para cada barra, se obtiene su altura (yval), y se coloca un texto sobre la barra que muestra el valor 
-    del consumo en kWh.
-    """
-    for bar in bars:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width() / 2, yval, f'{yval:.0f} kWh', va='bottom', ha='center', fontsize=14,
-                 color='black')
+    elif chart_type == 'line':
+        plt.plot(appliances, energies, marker='o', color='#3498db', linewidth=2)
+        plt.xlabel('Electrodoméstico', fontsize=14)
+        plt.ylabel('Consumo en kWh', fontsize=14)
+        plt.title('Consumo Energético - Línea', fontsize=16, fontweight='bold')
 
-    # Se establece el límite superior del eje Y para que sea un 20% mayor que el consumo máximo,
-    # lo que proporciona un espacio adicional en la parte superior del gráfico.
-    plt.ylim(0, max(energies) * 1.2)
-    # Se añade una cuadrícula horizontal al gráfico para facilitar la lectura de los valores.
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    elif chart_type == 'pie':
+        plt.pie(energies, labels=appliances, autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors)
+        plt.title('Consumo Energético - Torta', fontsize=16, fontweight='bold')
 
-    # Si no existe el directorio output, se crea.
-    # Se define el nombre del archivo para guardar el gráfico.
-    # Se utiliza plt.savefig() para guardar el gráfico en un archivo con una resolución de 300 dpi y
-    # ajustando los márgenes.
-    # Después de guardar el gráfico, se cierra la figura con plt.close() para liberar memoria.
+    # Guardar el gráfico en un archivo PNG único
     if not os.path.exists('output'):
         os.makedirs('output')
 
-    # Generar un UUID para el nombre del archivo
     unique_id = str(uuid.uuid4())
-    filename = f'consumo_{unique_id}.png'  # Nombre único con UUID
+    filename = f'consumo_{chart_type}_{unique_id}.png'
     filepath = os.path.join('output', filename)
     plt.savefig(filepath, dpi=300, bbox_inches='tight')
     plt.close()
-    print(f"Gráfico guardado en {filepath}")
 
     return filename
 
